b6f9730154ad6ad5fdd60d356cdb88e9af2e3bce
Joshua Cotton <jcotton42@outlook.com>
2020-05-15 19:31:01 -0400

    Basic fault handling

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,14 @@ use blog_os::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello world{}", "!");
 
+    blog_os::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
